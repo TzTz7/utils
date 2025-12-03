a9873f3d15a9dc904f4043f2ab7e963b6b9a746a
tz7 <nxba9b@foxmail.com>
2025-12-03 14:00:53 +0800

    update

--- unset_proxy.sh
+++ unset_proxy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
+unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy ALL_PROXY all_proxy NO_PROXY no_proxy
 
 git config --global --unset http.proxy
 git config --global --unset https.proxy
